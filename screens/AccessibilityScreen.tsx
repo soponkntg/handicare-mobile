9085a80e1a5166d0cb666e1219aa33b41864bd08
Siraphop Thaisangsa-nga <siraphop.fu@gmail.com>
2022-04-24 22:33:01 +0700

    add accessibility: undefined

--- screens/AccessibilityScreen.tsx
+++ screens/AccessibilityScreen.tsx
@@ -21,6 +21,8 @@ export default function AccessibilityScreen({
   navigation,
   route,
 }: MainStackScreenProps<"Accessibility">) {
+  console.log(route.params);
+
   const [elevatorExpanded, setElevatorExpanded] = React.useState(true);
   const [parkingExpanded, setParkingExpanded] = React.useState(true);
   const [toiletExpanded, setToiletExpanded] = React.useState(true);
@@ -33,9 +35,19 @@ export default function AccessibilityScreen({
   const doors: DoorType[] = route.params.doors;
   const toilets: ToiletType[] = route.params.toilets;
 
+  const isElevatorsExisted: boolean = elevators.length > 0;
+  const isParkingsExisted: boolean = parkings.length > 0;
+  const isRampsExisted: boolean = ramps.length > 0;
+  const isDoorsExisted: boolean = doors.length > 0;
+  const isToiletsExisted: boolean = toilets.length > 0;
+
   const displayText = (str: string) => {
-    return str.length > 0 ? str : "-";
+    return str.length > 0 ? str : undefined;
   };
+
+  const displayTextAlt = (str: string) => {
+    return str.length > 0 ? str : "-";
+  }
   const displaySwitch = (b: boolean) => {
     return b ? "Switch" : "No Usable Switch";
   };
@@ -49,10 +61,14 @@ export default function AccessibilityScreen({
     return b ? "Near entry" : "Not near entry";
   };
 
+  const displayPassable = (b: boolean) => {
+    return b ? "Passable" : "Not Passable";
+  };
+
   const elevatorsList = elevators.map((item) => {
     return (
       <ElevatorDetail
-        location={displayText(item.located)}
+        location={displayTextAlt(item.located)}
         button={displaySwitch(item.switch)}
         remark={displayText(item.remark)}
         key={"e" + item.id}
@@ -64,7 +80,7 @@ export default function AccessibilityScreen({
     return (
       <ParkingDetail
         key={"p" + item.id}
-        location={displayText(item.located)}
+        location={displayTextAlt(item.located)}
         floor={displayText(item.floor)}
         door={displayNearEntry(item.nearEntry)}
         car={displayEnoughSpace(item.enoughSpace)}
@@ -77,7 +93,7 @@ export default function AccessibilityScreen({
     return (
       <ToiletDetail
         key={"t" + item.id}
-        location={displayText(item.located)}
+        location={displayTextAlt(item.located)}
         floor={displayText(item.floor)}
         type={displayText(item.type)}
         door={displayText(item.doorType)}
@@ -91,7 +107,7 @@ export default function AccessibilityScreen({
     return (
       <RampDetail
         key={"r" + item.id}
-        location={displayText(item.located)}
+        location={displayTextAlt(item.located)}
         floor={displayText(item.floor)}
         slope={displayText(item.slope)}
         level={item.level.toString()}
@@ -105,10 +121,11 @@ export default function AccessibilityScreen({
     return (
       <DoorDetail
         key={"d" + item.id}
-        location={item.located.length > 0 ? item.located : "-"}
-        floor={item.floor.length > 0 ? item.floor : "-"}
-        door={item.doorType.length > 0 ? item.doorType : "-"}
-        remark={item.remark.length > 0 ? item.remark : "-"}
+        location={displayTextAlt(item.located)}
+        floor={displayText(item.floor)}
+        door={displayText(item.doorType)}
+        accessibility={displayPassable(item.passable)}
+        remark={displayText(item.remark)}
       />
     );
   });
@@ -118,7 +135,7 @@ export default function AccessibilityScreen({
       <Text style={styles.title} bold>
         Accessibilities
       </Text>
-      {elevatorsList.length > 0 && (
+      {isElevatorsExisted && (
         <View style={styles.cardMargin}>
           <Pressable
             style={elevatorExpanded ? styles.header_expanded : styles.header}
@@ -140,7 +157,7 @@ export default function AccessibilityScreen({
           </View>
         </View>
       )}
-      {parkingsList.length > 0 && (
+      {isParkingsExisted && (
         <View style={styles.cardMargin}>
           <Pressable
             style={parkingExpanded ? styles.header_expanded : styles.header}
@@ -162,7 +179,7 @@ export default function AccessibilityScreen({
           </View>
         </View>
       )}
-      {toiletsList.length > 0 && (
+      {isToiletsExisted && (
         <View style={styles.cardMargin}>
           <Pressable
             style={toiletExpanded ? styles.header_expanded : styles.header}
@@ -184,7 +201,7 @@ export default function AccessibilityScreen({
           </View>
         </View>
       )}
-      {rampsList.length > 0 && (
+      {isRampsExisted && (
         <View style={styles.cardMargin}>
           <Pressable
             style={rampExpanded ? styles.header_expanded : styles.header}
@@ -206,7 +223,7 @@ export default function AccessibilityScreen({
           </View>
         </View>
       )}
-      {doorsList.length > 0 && (
+      {isDoorsExisted && (
         <View style={styles.cardMargin}>
           <Pressable
             style={doorExpanded ? styles.header_expanded : styles.header}
@@ -265,6 +282,5 @@ const styles = StyleSheet.create({
   },
   hide: {
     display: "none",
-    
   },
 });
